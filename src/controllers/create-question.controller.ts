3f9bf55feb902f73bc683226db7724dc1b549007
Thiago Leite <leite.tjs@gmail.com>
2023-09-21 05:31:37 -0300

    feat: decorator to retrieve current user

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Post, UseGuards } from '@nestjs/common'
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common'
+import { CurrentUser } from 'src/auth/current-user.decorator'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { UserPayload } from 'src/auth/jwt-strategy'
 import { PrismaService } from 'src/prisma/prisma.service'
 import { z } from 'zod'
 
-const createQuestionBodySchema = z.object({})
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+})
+
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
 
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
@@ -11,7 +18,12 @@ export class CreateQuestionController {
   constructor(private readonly prisma: PrismaService) {}
 
   @Post()
-  async handle() {
-    return 'ok'
+  async handle(
+    @Body() body: CreateQuestionBodySchema,
+    @CurrentUser() user: UserPayload,
+  ) {
+    const { title, content } = body
+
+    return user
   }
 }
