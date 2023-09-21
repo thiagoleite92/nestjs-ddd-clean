e3167e3d16c50cddb230d6f5decd8cf6d2c58f44
Thiago Leite <leite.tjs@gmail.com>
2023-09-21 14:27:29 -0300

    test: create account test

--- src/controllers/create-account.controller.e2e-spec.ts
+++ src/controllers/create-account.controller.e2e-spec.ts
@@ -1,5 +1,38 @@
-describe('', () => {
-  it('', () => {
-    expect(2 + 2).toBe(4)
+import { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+
+import { AppModule } from '@/app.module'
+import { PrismaService } from '@/prisma/prisma.service'
+
+describe('Create Account (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    prisma = moduleRef.get(PrismaService)
+
+    await app.init()
+  })
+
+  test('[POST] /accounts', async () => {
+    const response = await request(app.getHttpServer()).post('/accounts').send({
+      name: 'Jonas',
+      email: 'jonas@gmail.com',
+      password: 'Senha@123',
+    })
+
+    expect(response.statusCode).toBe(201)
+
+    const userOnDataBase = await prisma.user.findUnique({
+      where: { email: 'jonas@gmail.com' },
+    })
+
+    expect(userOnDataBase).toBeTruthy()
   })
 })
