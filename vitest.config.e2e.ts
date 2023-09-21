46fc161d0be831ad68d7906b4235b8ba6361234b
Thiago Leite <leite.tjs@gmail.com>
2023-09-21 11:27:54 -0300

    test: setup single db tests

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -4,10 +4,11 @@ import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   test: {
+    root: './',
+    globals: true,
     include: ['**/*.e2e-spec.ts'],
     exclude: ['**/data/pg/**', 'node_modules'],
-    globals: true,
-    root: './',
+    setupFiles: ['./test/setup-e2e.ts'],
   },
   plugins: [
     tsConfigPaths(),
